f3c61aae63a83bb36b84fb2a1a5287b1a3e34212
cyl3el2cleal2 <nathachian@gmail.com>
2019-10-16 17:39:11 +0700

    Add RecycleView to ListCard

---
+++ app/src/main/java/buu/s59160937/savemycards/CardAdapter.kt
@@ -0,0 +1,28 @@
+package buu.s59160937.savemycards
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.model.view.*
+
+class CardAdapter(val items: Array<String>, val imageId: Array<Int>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        p0?.getNameTxt?.text = items.get(p1)
+        p0?.getThumbnail?.setImageResource(imageId.get(p1))
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.model,p0, false))
+    }
+}
+
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    val getNameTxt = view.nameTxt
+    val getThumbnail = view.thumbnail
+}
--- app/src/main/java/buu/s59160937/savemycards/ListCardFragment.kt
+++ app/src/main/java/buu/s59160937/savemycards/ListCardFragment.kt
@@ -6,12 +6,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import buu.s59160937.savemycards.databinding.FragmentListCardBinding
 
 
 class ListCardFragment : Fragment() {
+    var recyclerView: RecyclerView? = null
 
 
+    var foods = arrayOf(
+        "Minced pork omelette",
+        "Stir fried pork with basil",
+        "Papaya salad",
+        "Boiled egg, Bael leaves",
+        "Pad Thai",
+        "Korat Noodle",
+        "Shrimp Salad",
+        "Boiled pork noodles",
+        "Steamed sea",
+        "Steamed rices",
+        "Pork Belly",
+        "Liang Vegetable Fried Eggs"
+    )
+    var arrImg = arrayOf<Int>(
+        R.drawable.image1,
+        R.drawable.image2,
+        R.drawable.image3,
+        R.drawable.image4,
+        R.drawable.image5,
+        R.drawable.image6,
+        R.drawable.image7,
+        R.drawable.image8,
+        R.drawable.image9,
+        R.drawable.image10,
+        R.drawable.image11,
+        R.drawable.image12
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,7 +50,16 @@ class ListCardFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentListCardBinding>(
             inflater, R.layout.fragment_list_card, container, false
         )
+
+        binding.listCard.layoutManager = LinearLayoutManager(activity)
+
+        val cardAdapter = context?.let { CardAdapter(foods, arrImg, it) }
+        binding.listCard.adapter = cardAdapter
+
+
         return binding.root
     }
+
+
 
 }
